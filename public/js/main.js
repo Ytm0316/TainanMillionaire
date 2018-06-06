1f3aa478edd20f7c9d3fcbab0348498fad321d35
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-06 23:22:52 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -9,25 +9,45 @@ file above might has different path.  not case sensitive of jsfiles...
 
 must be imported by index.html 
 
-*/
+*/			var light_helper;
+			var light;
+			var light2;
+			var light2_helper;
+			var ambient;
 			var gui;
 			var gridHelper ;
 			var axes_helper ; 
 			var scene ;
 			var camera ;
 			var renderer ;
-			var loader ;	
+			var loader ;
+			var	light ;	
+			var light_helper;			
 			var cube=[];// 0 for jumper .
+			var cube_color_order=1;
 			var frustumSize = 200;
 			var jump_info={
+				gaming:true,//temp
+				landing_success:true,
+				at_cube:1,
 				ready:false,//click?
 				fly: false,
 				horizontal_speed: 0,
 				vertical_speed:true,//true for rising,false for falling
 				count:0,
 				cheer:function(){
-					if(this.horizontal_speed<50)//speed limit
-					this.horizontal_speed+=0.2;
+					if(this.horizontal_speed<50){
+						if(this.horizontal_speed<10)
+							this.horizontal_speed+=0.2;
+						else if(this.horizontal_speed<20)
+							this.horizontal_speed+=0.25;
+						else if(this.horizontal_speed<30)
+							this.horizontal_speed+=0.3;
+						else if(this.horizontal_speed<40)
+							this.horizontal_speed+=0.35;
+						else if(this.horizontal_speed<50)
+							this.horizontal_speed+=0.4;
+					}//speed limit
 				},
 				flying:function(){
 					this.count++;
@@ -36,14 +56,45 @@ must be imported by index.html
 					}
 					if(this.count==17){
 						//cube[0].position.y = 0;
-						cube[0].rotation.y = 0;
+						//cube[0].rotation.y = 0;
 						this.horizontal_speed = 0;
 						this.count = 0;
 						this.fly = false;
 						this.vertical_speed = true;
 						this.ready = false;
+						this.landing();//check if landing successfully.
 					}
 					
+				},
+				landing:function(){
+					if(jump_info.at_cube!=(cube.length-1)){//endpoint no need to search for next
+					//note that if it's endpoint ,then game should be finished
+						x_max = cube[this.at_cube+1].x_max;
+						x_min = cube[this.at_cube+1].x_min;
+						z_max = cube[this.at_cube+1].z_max;
+						z_min = cube[this.at_cube+1].z_min;
+					}
+					///next
+					x_cmax = cube[this.at_cube].x_max;
+					x_cmin = cube[this.at_cube].x_min;
+					z_cmax = cube[this.at_cube].z_max;
+					z_cmin = cube[this.at_cube].z_min;
+					///current
+					x_c = cube[0].position.x;
+					z_c = cube[0].position.z;
+					if(((jump_info.at_cube!=(cube.length-1))&&(x_c<=x_max)&&(x_c>=x_min)&&(z_c<=z_max)&&(z_c>=z_min))){
+						this.at_cube++;
+					}
+					else if(((x_c<=x_cmax)&&(x_c>=x_cmin)&&(z_c<=z_cmax)&&(z_c>=z_cmin))){
+						
+					}
+					else{
+						this.landing_success = false ;
+					}
+					
+				},
+				relife:function(){
+					this.landing_success = true;
 				}
 			};
 			
@@ -54,31 +105,54 @@ must be imported by index.html
 			
 			function animate(){
 			
-			if(jump_info.fly){
-				//do something here
-				cube[0].position.x += jump_info.horizontal_speed ;//這裡暫時使用x 這跟關卡地圖的跳躍角度有關
-						
-				if(jump_info.count == 8){
-					
-				}
-				else if(jump_info.vertical_speed){// vertical 
+			if(jump_info.landing_success){
+				if(jump_info.fly){
+				
+						if(cube[jump_info.at_cube].jump_direction==0){// jump left
+							cube[0].position.z -= jump_info.horizontal_speed*0.15 ;
+							cube[0].rotation.x -= Math.PI*2/17;//spin  i found that will lose one frame,causing rotation. wrong... then i correct it
+							//while(cube[0].rotation.x<0){cube[0].rotation.x += 2*Math.PI;}
+						}
+						else if(cube[jump_info.at_cube].jump_direction==1){// jump forward
+							cube[0].position.x += jump_info.horizontal_speed*0.15 ;		
+							cube[0].rotation.y += Math.PI*2/17;
+							//while(cube[0].rotation.y>2*Math.PI){cube[0].rotation.y -= 2*Math.PI;}
+						}
+						else if(cube[jump_info.at_cube].jump_direction==2){// jump right
+							cube[0].position.z += jump_info.horizontal_speed*0.15 ;
+							cube[0].rotation.x += Math.PI*2/17;
+							//while(cube[0].rotation.x2*Math.PI){cube[0].rotation.x -= 2*Math.PI;}							
+						}
+					if(jump_info.count == 8){
+					//stop frame	
+					}
+					else if(jump_info.vertical_speed){// vertical 
 					cube[0].position.y += 10;
-				}
-				else {
-					cube[0].position.y -= 10;
-				}
-				
-				
-				cube[0].rotation.y += Math.PI/9;		
-				jump_info.flying();	
+					}
+					else {
+						cube[0].position.y -= 10;
+					}						
+					jump_info.flying();	
 				
 				//camera follow
-				camera.position.set((cube[0].position.x -125) ,200,(cube[0].position.z +125 ));
-				camera.lookAt(  new THREE.Vector3((cube[0].position.x+75),0,cube[0].position.z-75) );
+					light.position.set((cube[0].position.x -25) ,300,(cube[0].position.z +25 ));
+					light2.position.set((cube[0].position.x -25) ,250,(cube[0].position.z +225 ))
+					camera.position.set((cube[0].position.x -125) ,200,(cube[0].position.z +125 ));
+					camera.lookAt(  new THREE.Vector3((cube[0].position.x+75),0,cube[0].position.z-75) );
+				}
+			
+				if(jump_info.ready){//increase horizontal speed
+					jump_info.cheer();
+				}
 			}
-			if(jump_info.ready){//increase horizontal speed
-				jump_info.cheer();
+			else{// fail to land.
+				if(jump_info.gaming){
+					alert('NOOB');
+					jump_info.gaming = false;
+				}
+				
 			}
+
 			
 			requestAnimationFrame(animate);//call jump success or fail animation here
 			renderer.render(scene,camera);
@@ -86,49 +160,121 @@ must be imported by index.html
 			//cube[0].rotation.x+=0.1;//原本Scene的坐標軸便不再適用，該物體會有自己的一個旋轉軸
 			//cube[0].rotation.z+=0.1;//調變該物體position時則採用Scene的坐標軸，而非自己的旋轉軸
 			
-			}
+			}// end of animate
 			function init(){
-				/*
-				(function(){
-					gui={}; 
-					var datGui = new dat.GUI();//add control plane. no use
-				})();
-				*/
+				var aspect = window.innerWidth / window.innerHeight;
+				renderer = new THREE.WebGLRenderer();
+				scene = new THREE.Scene();
+				loader = new THREE.OBJLoader();
 				gridHelper = new THREE.GridHelper( 1000, 20 );
 				axes_helper = new THREE.AxesHelper(20);
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color(0x000000);
+				camera = new THREE.OrthographicCamera( frustumSize * aspect / - 1, frustumSize * aspect / 1, frustumSize / 1, frustumSize / - 1, 1, 2000 );			
+				ambient = new THREE.AmbientLight(0xfffd87);
+				light = new THREE.PointLight( 0xffffff, 2, 500);
+				light2 = new THREE.PointLight( 0xffffff, 1, 500);
+				light_helper = new THREE.CameraHelper( light.shadow.camera );
+				light2_helper = new THREE.CameraHelper( light2.shadow.camera );
+				scene.background = new THREE.Color(0x4286f4);
+				light.position.set( -100, 300, 100 );
+				light.castShadow = true;
+				light2.position.set( -100, 250, 300 );
+				light2.castShadow = true;
+				
+				
+				//-----------------------------------------------
+				light.shadow.mapSize.width = 512;  // default
+				light.shadow.mapSize.height = 512; // default
+				light.shadow.camera.near = 0.5;       // default
+				light.shadow.camera.far = 500      // default
+				
+				light2.shadow.mapSize.width = 512;  // default
+				light2.shadow.mapSize.height = 512; // default
+				light2.shadow.camera.near = 0.5;       // default
+				light2.shadow.camera.far = 500      // default
+				
+			
+				//-----------------------------------------------
 				/*
 				camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 				// use orthogono camera might be better for our game.
 				*/
-				var aspect = window.innerWidth / window.innerHeight;
-				camera = new THREE.OrthographicCamera( frustumSize * aspect / - 1, frustumSize * aspect / 1, frustumSize / 1, frustumSize / - 1, 1, 2000 );
 				
-				renderer = new THREE.WebGLRenderer();
-				loader = new THREE.OBJLoader();
+				
+				
+				renderer.shadowMap.enabled = true;
+				renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+				renderer.setPixelRatio( window.devicePixelRatio );
 				renderer.setSize( window.innerWidth, window.innerHeight );
 				document.body.appendChild( renderer.domElement );
-				scene.add( axes_helper);
-				scene.add( gridHelper );
+				
+
 				camera.position.set(-200,200,200);
 				camera.lookAt(new THREE.Vector3(0,0,0));
-
-				
-				
+				//make some cube here. only to demo//////////////////
+				var bg = new THREE.BoxGeometry(50,20,50);
+				var m1 = new THREE.MeshStandardMaterial({color: 0x00ff00});
+				var cb1 = new THREE.Mesh(bg,m1);	
+				var cb2 = new THREE.Mesh(bg,m1);
+				var	cb3 = new THREE.Mesh(bg,m1);			
+				scene.add(cb1);	scene.add(cb2);scene.add(cb3);
+				cb1.position.set(-75,-10,75);
+				cb2.position.set(0,-10,75);
+				cb3.position.set(0,-10,150);
+				cb1.receiveShadow = true;
+				cb2.receiveShadow = true;
+				cb3.receiveShadow = true;
+				cube[1] = { x_max:-50,x_min:-100,z_max:100,z_min:50,jump_direction:1,related: cb1};
+				cube[2] = { x_max:25,x_min:-25,z_max:100,z_min:50,jump_direction:2,related:	cb2};
+				cube[3] = { x_max:25,x_min:-25,z_max:175,z_min:125,jump_direction:1,related: cb3};
+				///////////////////////////////////////////////
 				quickLoad('obj/horse.obj',0,
 				function(obj){
 					obj.position.set(-75,0,75);
 					obj.rotation.x = -Math.PI/2;
+
 					obj.traverse(
 						function(child){
-							if(child instanceof THREE.Mesh){ 
-								child.material = new THREE.MeshBasicMaterial( {color: 0xa5e587} );
+							if(child instanceof THREE.Mesh){//child.material = new THREE.MeshBasicMaterial( {color: 0xa5e587} );
+								child.castShadow = true; //default is false
+								child.receiveShadow = false; //default
+								if(cube_color_order==1){
+									child.material = new THREE.MeshStandardMaterial( {color: 0xff0000} );
+									cube_color_order++;
+								}
+								
+								else if(cube_color_order==5){
+									child.material = new THREE.MeshStandardMaterial( {color: 0xffff00} );
+									cube_color_order++;
+									
+								}
+								
+								else if(cube_color_order==4){
+									child.material = new THREE.MeshStandardMaterial( {color: 0xffbf00} );
+									cube_color_order++;
+								}
+								
+								else if(cube_color_order==11||cube_color_order==12){
+									child.material = new THREE.MeshStandardMaterial( {color: 0xffffff} );
+									cube_color_order++;
+								}
+			
+								else{
+									child.material = new THREE.MeshStandardMaterial( {color: 0x232323} );
+									cube_color_order++;
+								}
+								
 							}
 						});
 				});
-					window.addEventListener( 'resize', onWindowResize, false );
-		
+				window.addEventListener( 'resize', onWindowResize, false );
+				scene.add(ambient);
+				scene.add( light );
+				scene.add( light_helper );
+				scene.add( axes_helper);
+				scene.add( gridHelper );
+				scene.add( light2);
+				scene.add( light2_helper );
+				
 			}
 		function touchdown(){
 			if(!jump_info.fly)	jump_info.ready = true; //ready to fly,increase speed
@@ -153,6 +299,7 @@ must be imported by index.html
 					who can make the horse 's rotation point in the center.Please help
 				*/
 				callback(object);
+				cube_color_order=1;//this may cause problem..
 				cube[num] = object;
 				scene.add(cube[num]);
 			},
---
+++ public/page.js
@@ -0,0 +1,55 @@
+//variable
+
+var ifstart=0;
+
+var temporarybackground=document.getElementById("temporarybackground");
+var logo=document.getElementById("logo");
+var slogan=document.getElementById("slogan");
+var fb=document.getElementById("fb");
+var skip=document.getElementById("skip");
+var sign=document.getElementById("sign");
+var signintitle=document.getElementById("signintitle");
+var Email=document.getElementById("email");
+var Password=document.getElementById("password");
+var signin=document.getElementById("signin");
+var createaccount=document.getElementById("createaccount");
+var welcome=document.getElementById("welcome");
+var welcomepagesubtitle=document.getElementById("welcomepagesubtitle");
+var example=document.getElementById("example");
+var go=document.getElementById("go");
+
+logo.display="block";
+slogan.style.display="block";
+fb.style.display="block";
+skip.style.display="block";
+sign.style.display="block";
+signintitle.style.display="none";
+Email.style.display="none";
+Password.style.display="none";
+signin.style.display="none";
+createaccount.style.display="none";
+welcome.style.display="none";
+welcomepagesubtitle.style.display="none";
+example.style.display="none";
+go.style.display="none";
+
+skip.onclick = function(){
+	logo.style.display="none";
+	slogan.style.display="none";
+	fb.style.display="none";
+	skip.style.display="none";
+	sign.style.display="none";
+	welcome.style.display="block";
+	welcomepagesubtitle.style.display="block";
+	example.style.display="block";
+	go.style.display="block";
+}
+
+go.onclick = function(){
+	temporarybackground.style.display="none";
+	welcome.style.display="none";
+	welcomepagesubtitle.style.display="none";
+	example.style.display="none";
+	go.style.display="none";
+	ifstart=1;
+}
