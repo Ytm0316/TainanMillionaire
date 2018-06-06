0220ae83ddae61ad89653494c4ce3a183c1d5663
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-07 00:38:37 +0800

    Create reborn.js

---
+++ public/js/reborn.js
@@ -0,0 +1,14 @@
+var question = new Array(4);
+
+question[0] = "A" ;
+question[1] = "B" ;
+question[2] = "C" ;
+question[3] = "D" ;
+question[4] = "E" ;
+
+
+
+var RandNum = Math.floor(Math.random()*5); //←數字請填寫圖片張數的值
+var q=question[RandNum];
+document.write("<div class=/"content/">+q
+		</div>")
