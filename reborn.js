ec4d2ccbb1da599e09b7996dcb3cbe5854356a08
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-07 00:33:30 +0800

    Update reborn.js

--- reborn.js
+++ reborn.js
@@ -10,7 +10,5 @@ question[4] = "E" ;
 
 var RandNum = Math.floor(Math.random()*5); //←數字請填寫圖片張數的值
 var q=question[RandNum];
-document.write(<div class="content">赤崁樓位於哪一座城市
-		<p></p><p></p>
-		A:台南 B:台北 C:台中 D:高雄
-		</div>)
+document.write("<div class=/"content/">+q
+		</div>")
