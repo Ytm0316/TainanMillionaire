42e6ce5244410a5da0285e1248c1c1e30077b73c
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-07 22:57:56 +0800

    Update reborn.js

--- public/js/reborn.js
+++ public/js/reborn.js
@@ -8,9 +8,8 @@ function reborn(){
   question[4] = "E" ;
 
   var RandNum = Math.floor(Math.random()*5); //←數字請填寫圖片張數的值
-  var q=question[RandNum];
   var content=document.getElementById("content" + (RandNum));
-      content.innerHTML = q;
+      content.innerHTML = question[RandNum];
 
   
 }
