81ec7ab00acd80f2d22cc0d420895f7e3369338b
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-24 17:26:42 +0800

    Update reborn.js

--- public/js/reborn.js
+++ public/js/reborn.js
@@ -1,9 +1,9 @@
- var k=5  ; //問題數
+ var k=12  ; //問題數
  var q_used= new Array(k-1);
  var i;
  var player_ans=0;
  var selected=0;
- var deadtime = k;  //總共可以死幾次(問題數)
+ var deadtime = 3;  //總共可以死幾次(問題數)
  var rebor=document.getElementById("reborn");
  var select1=document.getElementById("select1");
  var select2=document.getElementById("select2");
@@ -76,12 +76,21 @@ function reborn(){
     var question = new Array(k-1); 
   
     //問題的答案請放在數字%4+1的選項  ex:第0題答案為A  第5題答案為B
-    question[0] = "A" ; 
-    question[1] = "B" ;
-    question[2] = "C" ;
-    question[3] = "D" ;
-    question[4] = "E" ;
-
+    question[0] = "李旦原是_____的漢族富商" ; 
+    question[1] = "大明國和荷蘭東印度公司在_____年爆發澎湖之戰" ;
+    question[2] = "在_________附近，有顏思齊上岸的紀念碑?" ;
+    question[3] = "早期的臺南市西部是一片汪洋，稱之" ;
+   
+    question[4] = "鄭芝龍早年投靠在澳門經商的舅父黃程，習得葡萄牙語並且成為_____教徒" ;
+    question[5] = "當時三大武裝海商集團有顏思齊，鄭芝龍和____";
+    question[6] = "赤崁城是____人在臺灣發展時修築";
+    question[7] = "1543年，葡萄牙船隻在經過臺灣海峽時，有個船員偶然遙望，發現一個青蔥翠綠的海島，禁不住喊出「Ilha Formosa」，就這樣，______，就成了西方世界對臺灣這個島嶼的稱呼。";
+    
+    question[8] = "鄭芝龍教名為?";
+    question[9] = "荷人興築商館並拓展其規模成為西式水岸堡壘，在台江西岸的一鯤鯓沙洲興建______，做為全島的軍事中心。";
+    question[10] = "荷蘭殖民當局為了開墾土地而急需勞動力，又缺乏本國移民發展殖民地，便招引大量___人移民臺灣。";
+    question[11] = "荷蘭獨立戰爭爆發後，荷蘭人成立了荷蘭____公司。";
+    //問題
     var RandNum = Math.floor(Math.random()*k); //←數字請填寫圖片張數的值
     if(q_used[RandNum]== -1)
       {check();}
