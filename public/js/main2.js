815f5e18e16ebeba5e061ad1b4b5fcf6cef36ee8
sktofu <37304563+sktofu@users.noreply.github.com>
2018-06-24 16:46:04 +0800

    Update main2.js

--- public/js/main2.js
+++ public/js/main2.js
@@ -3,7 +3,8 @@ jump.init();//遊戲初始化
 jump.start();//可以進行跳動
 jump.addFailedFn(//遊戲添加功能範例
 	function(){
-		alert('NOOB');
-		this.revive();
+		reborn();	
+	//	alert('NOOB');
+	//	this.revive();
 	}
 );
